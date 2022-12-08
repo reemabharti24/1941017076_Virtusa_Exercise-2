4bda03d8ded42805db0164c536cb0a57cc809ad4
reemabharti24 <53610950+reemabharti24@users.noreply.github.com>
2022-12-08 22:55:08 +0530

    Update TaxCalculater.java

--- TaxCalculater.java
+++ TaxCalculater.java
@@ -1,5 +1,46 @@
 package Problems;
-
-public class TaxCalculater {
-
+public interface TaxCalculator {
+	public abstract void execute();
 }
+public class Humanity implements TaxCalculator {
+	private int basic_salary;
+
+	public Order(int basic_salary) {
+		this.basic_salary = basic_salary;
+	}
+
+	@Override
+	public void execute() {
+        HRA=(10/100)*basicsalary;
+
+	}
+}
+public class Logistic implements TaxCalculator {
+	private int basic_salary;
+
+	public Order(int basic_salary) {
+		this.basic_salary = basic_salary;
+	}
+
+	@Override
+	public void execute() {
+        HRA=(10/100)*basicsalary;
+
+	}
+}
+public class Department {
+	public static void main(String[] args) {
+		basic_salary basic_salary = new basic_salary();
+        
+		Humanity humanity = new Humanity(basic_salary);
+		Logistic logistic = new Logistic(basic_salary);
+		Humanity.execute();
+
+		humanity = new humanity(basic_salary);
+		logistic = new Logistic(basic_salary);
+		Logistic.execute();
+	}
+}
+
+
+
